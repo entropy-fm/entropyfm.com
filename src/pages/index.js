add0f55d0f82457fddf895497acf744aa88286c1
bingbongle <contact@defund12.org>
2021-01-11 19:15:56 -0500

    Add skeleton components for player and calendar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
+import Calendar from "../components/calendar"
+import Player from "../components/player"
+
 export default function Home() {
-  return <div>Hello, World!</div>
+  return (
+    <div>
+      <div>Entropy FM</div>
+      <Player />
+      <Calendar />
+    </div>
+  )
 }
