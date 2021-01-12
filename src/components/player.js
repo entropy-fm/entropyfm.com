7042b41a3d861ca84e27f368d9307cbac5ce9078
bingbongle <contact@defund12.org>
2021-01-11 19:46:38 -0500

    Add extremely basic player toggle

--- src/components/player.js
+++ src/components/player.js
@@ -1,7 +1,19 @@
 import React from "react"
 
-const Player = () => {
-  return <div>Player</div>
+class Player extends React.Component {
+  state = {
+    isPlaying: false,
+  }
+  toggleState = () => {
+    this.setState({ isPlaying: !this.state.isPlaying })
+  }
+  render = () => {
+    return (
+      <div onClick={this.toggleState}>
+        {this.state.isPlaying ? "Pause" : "Play"}
+      </div>
+    )
+  }
 }
 
 export default Player
