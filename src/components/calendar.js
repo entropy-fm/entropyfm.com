2590f268d0472640c7001c8fbd8f48aa073734de
bingbongle <contact@defund12.org>
2021-01-12 08:46:20 -0500

    Add basic calendar functionality

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,7 +1,61 @@
 import React from "react"
 
-const Calendar = () => {
-  return <div>Calendar</div>
+import { gapi } from "gapi-script"
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      events: [],
+    }
+  }
+
+  componentDidMount = () => {
+    this.getEvents()
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Calendar: {this.state.events.length}</div>
+        {this.state.events.map(event => (
+          <li key={event.id}>{event.summary}</li>
+        ))}
+      </div>
+    )
+  }
+
+  getEvents = () => {
+    let that = this
+    function start() {
+      gapi.client
+        .init({
+          apiKey: `${process.env.GOOGLE_CALENDAR_API_KEY}`,
+        })
+        .then(() =>
+          gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${process.env.GOOGLE_CALENDAR_ID}/events`,
+          })
+        )
+        .then(
+          response => {
+            let events = response.result.items
+            that.setState(
+              {
+                events,
+              },
+              () => {
+                console.log(that.state.events)
+              }
+            )
+          },
+          function (reason) {
+            console.log(reason)
+          }
+        )
+    }
+    gapi.load("client", start)
+  }
 }
 
 export default Calendar
