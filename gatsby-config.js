18ee41a8eac08490f6b37009f9d97631d642e94e
bingbongle <contact@defund12.org>
2021-01-13 18:06:40 -0500

    Favicon fix?

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,28 @@ require("dotenv").config({
 })
 
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#121212`,
+        theme_color: `#121212`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
 }
