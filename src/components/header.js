c624f41e0c501b7376f63f7a1265cdc82e6baddf
bingbongle <contact@defund12.org>
2021-01-13 20:08:48 -0500

    New font, new header

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
     return (
       <div>
         <img
-          src="header.png"
+          src="header-2.png"
           style={{
             width: "50%",
           }}
