21d8f0b8c8632b21b65dc1c794efcf4717af8eb4
bingbongle <contact@defund12.org>
2021-01-22 18:29:30 -0500

    Remove unused stuff in Layout.js

--- src/components/common/layout.js
+++ src/components/common/layout.js
@@ -14,21 +14,12 @@ const Layout = ({ children }) => {
         <meta name="description" content="todo" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         {/* CSS Includes */}
         {/* eslint-disable-next-line max-len*/}
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,600;1,400&display=swap"
           rel="stylesheet"
         />
-
-        {/* JS Includes */}
-        {/* eslint-disable-next-line max-len*/}
-
-        {/* Google Maps - for geocoding letter addresses */}
-        {/* <script
-          src={`https://maps.googleapis.com/maps/api/js?key=${this.props.googleApiKey}&libraries=places`}
-        ></script> */}
       </Helmet>
       <Header />
       {children}
